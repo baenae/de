a99d72c7d1853cf6a1b92310271d58fe8676cdd9
Benjamin Sasse <benjamin.sasse@engram.de>
2021-11-25 15:59:20 +0100

    aktueller stand

--- src/de/app.js
+++ src/de/app.js
@@ -1,5 +1,12 @@
 $(document).ready(function () {
-	openPage('art');
+	openPhotoSubPage('photoindex');
+
+	if (location.search.match(new RegExp("tfp"))) {
+		openPage('tfp');
+	}
+	else {
+		openPage('start');
+	}
 });
 
 /**
@@ -13,6 +20,7 @@ function openPage(pageId) {
 	$('#art').css('display', 'none');
 	$('#contact').css('display', 'none');
 	$('#privatepolicy').css('display', 'none');
+	$('#tfp').css('display', 'none');
 
 	if (pageId === 'privatepolicy' || pageId === 'photography' || pageId === 'software' || pageId === 'art') {
 		$('#' + pageId).css('display', 'block');
@@ -21,4 +29,29 @@ function openPage(pageId) {
 	}
 
 	return false;
+}
+
+function openPhotoSubPage(pageID) {
+	$('#photoindex').css('display', 'none');
+	$('#composing').css('display', 'none');
+	$('#portraits').css('display', 'none');
+	$('#produkt').css('display', 'none');
+	$('#unterwegs').css('display', 'none');
+	$('#bremen').css('display', 'none');
+	$('#akt').css('display', 'none');
+
+	$('#' + pageID).css('display', 'block');
+}
+
+function makeHyperlinks() {
+	$('.imagegallery img').each(function (index){
+		var thumbSrc = ($(this).attr('src'));
+		var linkSrc = thumbSrc.replace('thumbnails/', '');
+		$(this).click(() => {
+			//console.log(linkSrc);
+
+			$('#modal').modal('show');
+			$('#img01').attr('src', linkSrc);
+		});
+	});
 }
